dc5172dced9b8428fef31754debb9131424f417e
networkSorcerer <gigattfyhig@gmail.com>
2024-12-31 12:38:59 +0900

    web crawling

--- 웹크롤링.py
+++ 웹크롤링.py
@@ -15,10 +15,34 @@ html = '''
             <td> 300 </td>
         </tr> 
     </table>
+    <ul>
+        <li> 100 </li>
+        <li> 200 </li>
+    </ul>
+    <ol>
+        <li> 300 </li>
+        <li> 400 </li>
+    </ol>
 </html> 
 '''
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.select('td'))
-rst = soup.select('td')
+# print(soup.select('td'))
+rst = soup.select('td') # td로 선택된 항목이 리스트 담김
 for e in rst:
-    
+    print(e.text, end=" ")
+print()
+
+rst2 = soup.select('ul li')
+for e in rst2:
+    print(e.text, end = " ")
+
+# 특정 요소 찾기(find와 find_all)
+# find : 조건에 맞는 첫 번째 요소를 반환
+# find_all : 조건에 맞는 모든 요소를 리스트로 반환
+
+import requests
+response = requests.get("https://naver.com")
+soup = BeautifulSoup(response.text, 'lxml')
+#print(soup)
+print(soup.find(attrs={"class":"notify_area"}))
+print(soup.find(attrs={"class":"shopping_area"}))
